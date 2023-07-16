f8dfd726c3d0bc64e2c632f1d56e04eb8d54cba0
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-07-15 21:26:05 -0600

    Add winning conditions logic

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const gameBoard = (() => {
+const GameBoard = (() => {
     let board = ["", "", "", "", "", "", "", "", ""];
 
     return {
@@ -6,20 +6,26 @@ const gameBoard = (() => {
     };
 })();
 
-const Player = (chosenMarker) => {
+const Player = (chosenMarker, myName) => {
     let marker = chosenMarker;
+    let name = myName;
+
+    const getMarker = () => marker;
+
+    const getName = () => name;
 
     return {
-        getMarker() {
-            return marker; 
-        }
+        getMarker,
+        getName
     };
 };
 
 const gameController = (() => {
-    const player1 = Player("X");
-    const player2 = Player("O");
+    let board = GameBoard.board;
+    const player1 = Player("X", "Player 1");
+    const player2 = Player("O", "Player 2");
     let currentPlayer = player1;
+    let result = "";
 
     const getCurrentPlayer = () => currentPlayer;
 
@@ -28,33 +34,78 @@ const gameController = (() => {
     };
 
     const addMarker = (index, player) => {
-        gameBoard.board[index] = player.getMarker();
+        board[index] = player.getMarker();
     };
 
     const playRound = (index) => {
-        if (gameBoard.board[index] !== "") return;
-        addMarker(index, getCurrentPlayer());
+        if (board[index] !== "") return;
+        let player = getCurrentPlayer();
+        addMarker(index, player);
+        checkGameOver(player);
         switchPlayer();
-        console.log(gameBoard.board);
+        console.log(board);
     };
 
+    const checkGameOver = (player) => {
+        let marker = player.getMarker();
+        let winningCombinations = [
+            board.slice(0,3),
+            board.slice(3,6),
+            board.slice(6,9),
+            board.filter((e, i) => i % 3 === 0),
+            board.slice(1).filter((e, i) => i % 3 === 0),
+            board.slice(2).filter((e, i) => i % 3 === 0),
+            [board[0], board[4], board[8]],
+            [board[2], board[4], board[6]]
+        ];
+        if (winningCombinations.some(combination =>
+            combination.every(value => value === marker))) endGame(player);
+        else if (!board.some(square => square === "")) endGame();
+    };
+
+    const endGame = (winner) => {
+        result = winner ? `${winner.getName()} wins!` : "It's a tie!"
+        console.log(result);
+        board.forEach((square, i) => {
+            if (square === "") board[i] = " ";
+        });
+    };
+
+    const resetGame = () => {
+        board = GameBoard.board;
+    };
+
+    const getResult = () => result;
+
     return {
-        playRound
+        playRound,
+        getCurrentPlayer,
+        switchPlayer,
+        getResult,
+        resetGame,
+        board
     };
 })();
 
 const displayController = (() => {
     const boardSquares = document.querySelectorAll(".board");
+    const text = document.getElementById("game-text");
 
     const showBoard = () => {
         boardSquares.forEach((square, i) => {
-            square.textContent = gameBoard.board[i];
+            square.textContent = gameController.board[i];
         });
+        text.textContent = `It's ${gameController.getCurrentPlayer().getName()}'s turn...`;
     };
 
     const clickHandler = (e) => {
         gameController.playRound(e.target.dataset.index);
         showBoard();
+        if (gameController.getResult() !== "") displayGameEnd();
+    };
+
+    const displayGameEnd = () => {
+        text.textContent = gameController.getResult();
     };
 
     boardSquares.forEach(square => {
@@ -65,8 +116,3 @@ const displayController = (() => {
 });
 
 displayController();
-
-// addMarker could go in game or player
-// .board query selector can inside renderBoard
-// renderBoard() can actually insert the elements, instead of just filling them, as just doing that doesn't make sense when the game starts
-// 
