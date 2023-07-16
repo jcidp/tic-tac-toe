2a25e6466c16d54aa547144a627fd5157c53977a
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-07-15 19:17:53 -0600

    Let players add markers to the board

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const gameBoard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
     return {
-        array: ["X", "X", "O", "O", "X", "X", "O", "O", "X"]
+        board
     };
 })();
 
-const Player = (marker) => {
-    //let marker = mark;
+const Player = (chosenMarker) => {
+    let marker = chosenMarker;
 
     return {
         getMarker() {
@@ -14,13 +16,57 @@ const Player = (marker) => {
     };
 };
 
-const displayController = (() => {
-    const renderBoard = () => {
-        document.querySelectorAll(".board").forEach((square, i) => {
-            square.textContent = gameBoard.array[i];
-        });
-    }
+const gameController = (() => {
+    const player1 = Player("X");
+    const player2 = Player("O");
+    let currentPlayer = player1;
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const addMarker = (index, player) => {
+        gameBoard.board[index] = player.getMarker();
+    };
+
+    const playRound = (index) => {
+        if (gameBoard.board[index] !== "") return;
+        addMarker(index, getCurrentPlayer());
+        switchPlayer();
+        console.log(gameBoard.board);
+    };
+
     return {
-        renderBoard
+        playRound
     };
 })();
+
+const displayController = (() => {
+    const boardSquares = document.querySelectorAll(".board");
+
+    const showBoard = () => {
+        boardSquares.forEach((square, i) => {
+            square.textContent = gameBoard.board[i];
+        });
+    };
+
+    const clickHandler = (e) => {
+        gameController.playRound(e.target.dataset.index);
+        showBoard();
+    };
+
+    boardSquares.forEach(square => {
+        square.addEventListener("click", clickHandler);
+    });
+
+    showBoard();
+});
+
+displayController();
+
+// addMarker could go in game or player
+// .board query selector can inside renderBoard
+// renderBoard() can actually insert the elements, instead of just filling them, as just doing that doesn't make sense when the game starts
+// 
